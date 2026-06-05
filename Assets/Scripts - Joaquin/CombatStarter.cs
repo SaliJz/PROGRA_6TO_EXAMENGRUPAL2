da1f3f7f6b1ea729e460803cea190995971bedce
Joaquin <166671976+SaliJz@users.noreply.github.com>
2026-06-05 18:21:10 -0500

    feat: Creación del CombatStarter para controlar la activación de los enemigos y el cambio de paneles.

---
+++ Assets/Scripts - Joaquin/CombatStarter.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class CombatStarter : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> enemyObjects;
+    [SerializeField] private List<Character> allCombatants;
+    [SerializeField] private GameObject combatPanel;
+    [SerializeField] private GameObject storyPanel;
+    private TurnManager turnManager;
+
+    void Start()
+    {
+        turnManager = FindAnyObjectByType<TurnManager>(FindObjectsInactive.Include);
+        // Enemigos desactivados al inicio
+        foreach (var e in enemyObjects) e.SetActive(false);
+    }
+
+    // Llamado por StoryManager al terminar los diálogos
+    public void BeginCombat()
+    {
+        foreach (var e in enemyObjects) e.SetActive(true);
+        storyPanel.SetActive(false);
+        combatPanel.SetActive(true);
+        turnManager.InitializeCombat(allCombatants);
+    }
+
+    public void OnCombatVictory()
+    {
+        foreach (var e in enemyObjects) e.SetActive(false);
+        combatPanel.SetActive(false);
+        FindObjectOfType<ResultPanel>().ShowVictory();
+    }
+
+    public void OnCombatDefeat()
+    {
+        combatPanel.SetActive(false);
+        FindObjectOfType<ResultPanel>().ShowDefeat();
+    }
+}
